8adc95873ec605bf9eca2ab80fc9cafd58524b4a
Nabeel-Q <145397359+Nabeel-Q@users.noreply.github.com>
2023-10-03 10:19:05 -0400

    Add files via upload

---
+++ week 4.py
@@ -0,0 +1,15 @@
+# loops
+
+# count = 0
+# while count < 100:
+#     print("programming is fun!")
+#     count = count + 1
+
+# sumation of series using while loop
+
+sum = 0
+i = 1
+while i < 10:
+    sum = sum + i
+    i = i + 1
+print("Sum is", sum)
